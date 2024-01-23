c72505ab0baf82109a556c36b6c18cf7c8e8d337
north238 <fumiyama02@yahoo.co.jp>
2024-01-24 05:24:36 +0900

    npm install finish

---
+++ html/my-assets/routes/admin.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Admin\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\Admin\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\Admin\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\Admin\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\Admin\NewPasswordController;
+use App\Http\Controllers\Auth\Admin\PasswordController;
+use App\Http\Controllers\Auth\Admin\PasswordResetLinkController;
+use App\Http\Controllers\Auth\Admin\RegisteredUserController;
+use App\Http\Controllers\Auth\Admin\VerifyEmailController;
+use App\Http\Controllers\Backend\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('backend.welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
+});
+
+Route::middleware('auth:admins')->group(function () {
+    // Route::get('/dashboard', function () {
+    //     return view('admin.dashboard');
+    // })->middleware('verified')->name('dashboard');
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/dashboard', 'index')->middleware('verified')->name('dashboard');
+    });
+
+    Route::get('verify-email', EmailVerificationPromptController::class)
+        ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware('throttle:6,1')
+        ->name('verification.send');
+
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->name('password.confirm');
+
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+    
+});
+
+// Route::middleware('auth:admins')->group(function () {
+//     Route::controller(AdminController::class)->group(function () {
+//         Route::get('/dashboard', 'index')->middleware('verified')->name('dashboard');
+//     });
+// });
